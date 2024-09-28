28d8e465583d8f5b2c5585f2a897a96e280c4261
Sadanand <sadachaudhari@gmail.com>
2024-09-28 11:57:59 +0530

    testimonial removed

--- components/PanelInstalled.js
+++ components/PanelInstalled.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import {  motion as m } from 'framer-motion'
-import { zoomIn } from '@/lib/animation'
+import React, { useEffect, useRef, useState } from "react";
+import { motion as m, useAnimation, useInView } from "framer-motion";
+import { zoomIn } from "@/lib/animation";
 
 const PanelInstalled = () => {
-  return (
-    <m.div variants={zoomIn(0.5,0.5)} initial="hidden" whileInView="show" viewport={{once:true, amount:0.3}} className="absolute bottom-10 left-10 md:bottom-10 bg-white/90 border border-white backdrop-blur md:-left-20 w-auto px-8 py-4 shadow-2xl flex flex-col rounded-md">
-            <m.p  className="text-5xl font-bold text-green-500 drop-shadow-md">250+</m.p>
-            <p className="text-gray-500 font-semibold ">Panels Installed</p>
-        </m.div>
-  )
-}
+  const [counter, setCounter] = useState(0);
+  const ref = useRef(null)
+  const isInView = useInView(ref);
+  useEffect(() => {
+    let interval;
+    if (counter < 250) {
+      interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 5);
+    }
+  console.log(isInView)
 
-export default PanelInstalled
+    return () => clearInterval(interval);
+  }, [counter,isInView]);
+
+  return (
+    <m.div ref={ref}
+      variants={zoomIn(0.5, 0.5)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.3 }}
+      className="absolute bottom-10 left-10 md:bottom-10 bg-white/90 border border-white backdrop-blur md:-left-20 w-auto px-8 py-4 shadow-2xl flex flex-col rounded-md"
+    >
+      <m.p className="text-5xl font-bold text-green-500 drop-shadow-md">
+        {counter}+
+      </m.p>
+      <p className="text-gray-500 font-semibold ">Panels Installed</p>
+    </m.div>
+  );
+};
+
+export default PanelInstalled;
--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Container from './Container'
+import React from "react";
+import Container from "./Container";
 
-
-import { FaStar } from 'react-icons/fa';
-import config from "../pages/index.json"
-import DottedAbstract from './common/DottedAbstract';
-import TopSubHeading from './common/heading/TopSubHeading';
-import DivTitle from './common/heading/DivTitle';
-import Image from 'next/image';
-import { motion as m } from 'framer-motion'
-import { fadeIn, slideIn } from '@/lib/animation'
+import { FaStar } from "react-icons/fa";
+import config from "../pages/index.json";
+import DottedAbstract from "./common/DottedAbstract";
+import TopSubHeading from "./common/heading/TopSubHeading";
+import DivTitle from "./common/heading/DivTitle";
+import Image from "next/image";
+import { motion as m } from "framer-motion";
+import { fadeIn, slideIn } from "@/lib/animation";
 
 // const testimonial = [
 //     {
@@ -50,40 +49,54 @@ import { fadeIn, slideIn } from '@/lib/animation'
 //   ];
 
 const Testimonial = () => {
-  const testimonials = config.testimonials
+  const testimonials = config.testimonials;
 
   return (
     <Container className="flex flex-col gap-20 items-center justify-center min-h-screen bg-gray-50 relative ">
-    <DottedAbstract className="-left-10 top-0"/>
-       <div className="flex flex-col gap-6 items-center">
-        <TopSubHeading text="What They Say About Us"/>
-          <DivTitle text="Customer Testimonials"/>
+      <DottedAbstract className="-left-10 top-0" />
+      <div className="flex flex-col gap-6 items-center">
+        <TopSubHeading text="What They Say About Us" />
+        <DivTitle text="Customer Testimonials" />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {testimonials.map((testimonial,idx) => 
-      <m.div key={idx} variants={fadeIn('up','tween',0.1*idx,0.3)} initial="hidden"  whileInView="show" viewport={{once:true, amount:0.3}} className="flex flex-col gap-6 p-8  rounded-md shadow-md border shadow-gray-100 bg-white">
-               <div className="flex text-green-400"><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></div>
-               <p className="text-gray-500">{testimonial.text}</p>
-               <div className="flex items-center  gap-4 mt-auto">
-                <Image width={80} height={80} 
+        {testimonials?.map((testimonial, idx) => (
+          <m.div
+            key={idx}
+            variants={fadeIn("up", "tween", 0.1 * idx, 0.3)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3 }}
+            className="flex flex-col gap-6 p-8  rounded-md shadow-md border shadow-gray-100 bg-white"
+          >
+            <div className="flex text-green-400">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+            <p className="text-gray-500">{testimonial.text}</p>
+            <div className="flex items-center  gap-4 mt-auto">
+              <Image
+                width={80}
+                height={80}
                 src={testimonial.image}
                 alt={testimonial.name}
                 className="w-14 h-14 object-cover rounded-full"
               />
               <div className="flex flex-col">
-                <p className="font-medium text-gray-900">
-                   {testimonial.name}
-                </p>
+                <p className="font-medium text-gray-900">{testimonial.name}</p>
                 <p className="text-green-500 text-sm">
                   {testimonial.designation}
                 </p>
-                </div>
-                </div>
-      </m.div> )}
+              </div>
+            </div>
+          </m.div>
+        ))}
       </div>
-  </Container>
-    )
-}
+    </Container>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
